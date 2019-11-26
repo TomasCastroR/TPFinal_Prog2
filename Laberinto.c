e0ba08e119723b60800486c4b7fcc7b06586ebb5
FranKKastor <FranKKastor@users.noreply.github.com>
2019-11-26 19:34:23 -0300

    Terminada la generacion del laberinto, falta verificadores y optimizar random

--- Laberinto.c
+++ Laberinto.c
@@ -1,6 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main (){
+#include <time.h>
+void InicializarLab (int *Laberinto[],int Dimension){
+    for(int i=0; i<Dimension;++i){
+        for(int j=0;j<Dimension;++j){
+            Laberinto[i][j]='0';
+        }
+    }
+}
+void LiberarMemoria (int *Array[],int Dimension){
+    for(int i=0;i<Dimension;++i){
+        free(Array[i]);
+    }
+    free(Array);
+}
+void ObstaculosRandom (FILE *Archivo,int *Laberinto[],int CantObsRandom,int Dimension){
+    int FilaRandom,ColumnaRandom;
+    srand(time(NULL));
+    for(int i=0;i<CantObsRandom;++i){
+        FilaRandom = rand()%Dimension;
+        ColumnaRandom = rand()%Dimension;
+        while(Laberinto[FilaRandom][ColumnaRandom]!='0'){
+            FilaRandom = rand()%Dimension;
+            ColumnaRandom = rand()%Dimension;
+        }
+        //printf("(%d,%d)\n",FilaRandom+1,ColumnaRandom+1);
+        Laberinto[FilaRandom][ColumnaRandom]='1';
+    }
+}
+void LayoutLab (FILE *Archivo,int *Laberinto[],int Dimension){
+    char buffer[100],Caracter;
+    int Fila,Columna,ObsRandom;
+    fgets(buffer,100,Archivo);
+    Caracter=fgetc(Archivo);
+    while(Caracter == '('){
+        fscanf(Archivo,"%d,%d)\n",&Fila,&Columna);
+        Laberinto[Fila-1][Columna-1]='1';
+        Caracter=fgetc(Archivo);
+    }
+    fgets(buffer,100,Archivo);
+    fscanf(Archivo,"%d\n",&ObsRandom);
+    fgets(buffer,100,Archivo);
+    fscanf(Archivo,"(%d,%d)\n",&Fila,&Columna);
+    Laberinto[Fila-1][Columna-1]='I';
+    fgets(buffer,100,Archivo);
+    fscanf(Archivo,"(%d,%d)\n",&Fila,&Columna);
+    Laberinto[Fila-1][Columna-1]='X';
+    ObstaculosRandom(Archivo,Laberinto,ObsRandom,Dimension);
+    for(int i=0; i<Dimension;++i){
+        for(int j=0;j<Dimension;++j){
+            printf("%c ",Laberinto[i][j]);
+        }
+        printf("\n");}
+}
+void Escritura (int *Laberinto[],int Dimension){
+    FILE *ArchivoSalida;
+    ArchivoSalida = fopen("Laberinto.txt","w");
+    for(int i=0;i<Dimension;++i){
+        for(int j=0;j<Dimension;++j){
+            fputc(Laberinto[i][j],ArchivoSalida);
+        }
+        fputc('\n',ArchivoSalida);
+    }
+    fclose(ArchivoSalida);
+}
     
+int main (){
+    FILE *Entrada;
+    char buffer[20];
+    int Dimension;
+    Entrada = fopen("ejemplo.txt","r");
+    fgets(buffer,20,Entrada);
+    fscanf(Entrada,"%d",&Dimension);
+    fgetc(Entrada);
+    int **Laberinto=(int**)malloc(sizeof(int*)*Dimension);
+    for(int i=0;i<Dimension;++i){
+        Laberinto[i]=malloc(sizeof(int)*Dimension);
+    }
+    InicializarLab(Laberinto,Dimension);
+    LayoutLab(Entrada,Laberinto,Dimension);
+    fclose(Entrada);
+    Escritura(Laberinto,Dimension);
+    LiberarMemoria(Laberinto,Dimension);
 }
