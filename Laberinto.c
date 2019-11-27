38bc51195ff103fbd76bc52d3d3179351a0dda65
FranKKastor <FranKKastor@users.noreply.github.com>
2019-11-27 18:48:35 -0300

    ahora si?

--- Laberinto.c
+++ Laberinto.c
@@ -15,6 +15,9 @@ void LiberarMemoria (int *Array[],int Dimension){
     }
     free(Array);
 }
+int Verificar(int PosX, int PosY,int Dimension, int *Laberinto[]){
+    return (PosX>0 && PosY>0)&&(PosX<=Dimension&&PosY<=Dimension)&&(Laberinto[PosX-1][PosY-1]=='0');
+}
 void ObstaculosRandom (FILE *Archivo,int *Laberinto[],int CantObsRandom,int Dimension){
     int FilaRandom,ColumnaRandom;
 
@@ -31,32 +34,38 @@ void ObstaculosRandom (FILE *Archivo,int *Laberinto[],int CantObsRandom,int Dime
         Laberinto[FilaRandom][ColumnaRandom]='1';
     }
 }
-void LayoutLab (FILE *Archivo,int *Laberinto[],int Dimension){
-    char buffer[100],Caracter;
-    int Fila,Columna,ObsRandom;
+int LayoutLab (FILE *Archivo,int *Laberinto[],int Dimension){
+    int Validez=1,Fila,Columna,ObsRandom,LargoBuffer=100;
+    char buffer[LargoBuffer],Caracter;
 
     fgets(buffer,100,Archivo);
     Caracter=fgetc(Archivo);
-    while(Caracter == '('){
+    while(Caracter == '('&&Validez==1){
         fscanf(Archivo,"%d,%d)\n",&Fila,&Columna);
-        Laberinto[Fila-1][Columna-1]='1';
-        Caracter=fgetc(Archivo);
+        if(Verificar(Fila,Columna,Dimension,Laberinto)==1){
+            Laberinto[Fila-1][Columna-1]='1';
+            Caracter=fgetc(Archivo);
+        }
+        else Validez=0;
     }
     fgets(buffer,100,Archivo);
     fscanf(Archivo,"%d\n",&ObsRandom);
+    fgets(buffer,100,Archivo);
+    fscanf(Archivo,"(%d,%d)\n",&Fila,&Columna);
+    if(Verificar(Fila,Columna,Dimension,Laberinto)==1){
+        Laberinto[Fila-1][Columna-1]='I';}
+    else Validez=0;
 
     fgets(buffer,100,Archivo);
     fscanf(Archivo,"(%d,%d)\n",&Fila,&Columna);
-    Laberinto[Fila-1][Columna-1]='I';
-
-    fgets(buffer,100,Archivo);
-    fscanf(Archivo,"(%d,%d)\n",&Fila,&Columna);
-    Laberinto[Fila-1][Columna-1]='X';
-
-    ObstaculosRandom(Archivo,Laberinto,ObsRandom,Dimension);
+    if(Verificar(Fila,Columna,Dimension,Laberinto)==1){
+        Laberinto[Fila-1][Columna-1]='X';}
+    else Validez=0;
+    if(Validez==1)ObstaculosRandom(Archivo,Laberinto,ObsRandom,Dimension);
+    return Validez;
 }
-void Escritura (int *Laberinto[],int Dimension){
-    FILE *ArchivoSalida = fopen("Laberinto.txt","w");
+void Escritura (int *Laberinto[],int Dimension,char NombreSalida[]){
+    FILE *ArchivoSalida = fopen(NombreSalida,"w");
 
     for(int i=0;i<Dimension;++i){
         for(int j=0;j<Dimension;++j){
@@ -66,23 +75,29 @@ void Escritura (int *Laberinto[],int Dimension){
     }
     fclose(ArchivoSalida);
 }
-int main (){
-    FILE *Entrada = fopen("ejemplo.txt","r");
-    char buffer[20];
-    int Dimension;
+int main (int Argc,char *Argumentos[]){
+    FILE *Entrada = fopen(Argumentos[1],"r");
+    int Dimension,LargoBuffer=20;
+    char buffer[LargoBuffer];
 
     fgets(buffer,20,Entrada);
-    fscanf(Entrada,"%d",&Dimension);
-    fgetc(Entrada);
+    fscanf(Entrada,"%d\n",&Dimension);
     int **Laberinto=(int**)malloc(sizeof(int*)*Dimension);
     for(int i=0;i<Dimension;++i){
         Laberinto[i]=malloc(sizeof(int)*Dimension);
     }
 
     InicializarLab(Laberinto,Dimension);
-    LayoutLab(Entrada,Laberinto,Dimension);
-    fclose(Entrada);
+    if(LayoutLab(Entrada,Laberinto,Dimension)==1){
+        fclose(Entrada);
+        Escritura(Laberinto,Dimension,Argumentos[2]);
+        LiberarMemoria(Laberinto,Dimension);
+    }
+    else{
+        printf("La entrada no es valida");
+        LiberarMemoria(Laberinto,Dimension);
+    }
 
-    Escritura(Laberinto,Dimension);
-    LiberarMemoria(Laberinto,Dimension);
+    
+    return 0;
 }
---
+++ ResolverLaberinto.py
@@ -0,0 +1,5 @@
+import subprocess
+
+def main():
+    Entrada = open(Argumentos[1],"r");
+    Laberinto = Entrada.readlines()
