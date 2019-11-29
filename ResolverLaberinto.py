3eab5a07cd7a467077eb4fbe8922094202a16912
FranKKastor <FranKKastor@users.noreply.github.com>
2019-11-28 23:01:08 -0300

    mañana consulta

--- ResolverLaberinto.py
+++ ResolverLaberinto.py
@@ -1,5 +1,11 @@
 import subprocess
-
+def ImprimirSolucion(Solucion):
+    if(Solucion!=[]):
+        for Pasos in Solucion:
+            print(Pasos)
+    else:
+        print("Este laberinto no tiene solucion") 
+        response = subprocess.run(["./a.exe","Entrada.txt"])
 def Encontrar(Laberinto):
     Fila=0
     Columna=0
@@ -14,12 +20,21 @@ def Encontrar(Laberinto):
         Columna=0
         Fila+=1
     return (Inicio,Final)
-def ResolverLaberinto(Inicio,Final):
+def ResolverLaberinto(Laberinto,Inicio,Final):
+    Visitados = [[False]*len(Laberinto)]*len(Laberinto)
+    Camino = [Inicio]
+    Visitados[Inicio[0]][Inicio[1]] = True
+    LlegarObjetivo = False
+    while(not LlegarObjetivo and Camino!=[]):
     
+
+    return Camino
+
 def main():
     Entrada = open("salida.txt","r")
     Laberinto =list(map(lambda linea:list(linea.strip()),Entrada.readlines()))
     Entrada.close()
     Salida_Objetivo=Encontrar(Laberinto)
-    ResolverLaberinto(Salida_Objetivo[0],Salida_Objetivo[1])
+    Recorrido = ResolverLaberinto(Laberinto,Salida_Objetivo[0],Salida_Objetivo[1])
+    ImprimirSolucion(Recorrido)
 main()
